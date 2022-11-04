9eb91b25956ab875442af86ba7fe5c8b91c0cc19
prostiate <prostiate@gmail.com>
2022-11-04 21:28:44 +0700

    feat: add authentication using nestjs/passport
    Register the auth module in app.module.
    apiKey.strategy is to validate the header that used passport
    auth.middleware control the session

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
+import { AuthMiddleware } from './middleware/auth/auth.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductsModule, CategoriesModule],
+  imports: [ProductsModule, CategoriesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('');
+  }
+}
